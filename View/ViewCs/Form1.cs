9ad854fd463cc10d07d454cad949f3dc92eb400b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-24 12:38:18 +0900

    削除される機能を呼び出している処理を無効化

--- View/ViewCs/Form1.cs
+++ View/ViewCs/Form1.cs
@@ -10,13 +10,7 @@ namespace ViewCs
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            System.String  message = TextBox1.Text;
-            int result;
-            SampleWrapper.Common.SampleDocument objWrapper = new SampleWrapper.Common.SampleDocument();
 
-            objWrapper.setMessage(message);
-            result = objWrapper.countAlphabet();
-            MessageBox.Show("文字列中のアルファベットの個数は " + result);
         }
     }
 
