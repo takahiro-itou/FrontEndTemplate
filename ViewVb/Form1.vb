6f2e37b94d7037c374659cdc5b084dc7d1596ceb
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-30 16:52:28 +0900

    リファクタリング

--- ViewVb/Form1.vb
+++ ViewVb/Form1.vb
@@ -1,14 +1,37 @@
 ﻿Public Class Form1
 
-    Private Sub RunCount(ByVal message As String)
+''========================================================================
+''    指定したコマンドを実行する。
+''========================================================================
+Private Sub RunCommand(ByVal command As String)
 
-    End Sub
+End Sub
 
-    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
-        RunCount(txtCommand.Text)
-    End Sub
+''========================================================================
+''    「実行」 ボタンのクリックイベントハンドラ。
+''
+''    入力したコマンドを実行する。
+''========================================================================
+Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles _
+            btnRun.Click
+    RunCommand(txtCommand.Text)
+End Sub
 
-    Private Sub mnuRunCommand_Click(sender As Object, e As EventArgs) Handles mnuRunCommand.Click
-        RunCount(txtCommand.Text)
-    End Sub
+''========================================================================
+''    メニュー「ファイル」－「終了」
+''========================================================================
+Private Sub mnuFileExit_Click(sender As Object, e As EventArgs) Handles _
+            mnuFileExit.Click
+    Application.Exit()
+End Sub
+
+''========================================================================
+''    メニュー「実行」－「コマンドを実行」
+''========================================================================
+
+Private Sub mnuRunCommand_Click(sender As Object, e As EventArgs) Handles _
+            mnuRunCommand.Click
+    RunCommand(txtCommand.Text)
+End Sub
+
 End Class
