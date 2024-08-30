d91557b64d5632c762a4bc791d4d33af6458ecfe
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-30 15:55:35 +0900

    リファクタリング
    (1) リソースファイルから値がセットされるプロパティへの
        コードでの直接代入は不要なので削除

--- ViewVb/Form1.Designer.vb
+++ ViewVb/Form1.Designer.vb
@@ -46,41 +46,29 @@ Partial Class Form1
         '
         resources.ApplyResources(Me.mnuMain, "mnuMain")
         Me.mnuMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuRun})
-        Me.mnuMain.Location = New System.Drawing.Point(0, 0)
         Me.mnuMain.Name = "mnuMain"
-        Me.mnuMain.Size = New System.Drawing.Size(516, 24)
-        Me.mnuMain.TabIndex = 4
-        Me.mnuMain.Text = "mnuMain"
         '
         ' mnuFile
         '
         resources.ApplyResources(Me.mnuFile, "mnuFile")
         Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFileExit})
         Me.mnuFile.Name = "mnuFile"
-        Me.mnuFile.Size = New System.Drawing.Size(37, 20)
-        Me.mnuFile.Text = "&File"
         '
         ' mnuFileExit
         '
         resources.ApplyResources(Me.mnuFileExit, "mnuFileExit")
         Me.mnuFileExit.Name = "mnuFileExit"
-        Me.mnuFileExit.Size = New System.Drawing.Size(180, 22)
-        Me.mnuFileExit.Text = "E&xit"
         '
         ' mnuRun
         '
         resources.ApplyResources(Me.mnuRun, "mnuRun")
         Me.mnuRun.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuRunCommand})
         Me.mnuRun.Name = "mnuRun"
-        Me.mnuRun.Size = New System.Drawing.Size(40, 20)
-        Me.mnuRun.Text = "&Run"
         '
         ' mnuRunCommand
         '
         resources.ApplyResources(Me.mnuRunCommand, "mnuRunCommand")
         Me.mnuRunCommand.Name = "mnuRunCommand"
-        Me.mnuRunCommand.Size = New System.Drawing.Size(180, 22)
-        Me.mnuRunCommand.Text = "&Count"
 
         '
         ' dlgOpen
@@ -92,39 +80,24 @@ Partial Class Form1
         ' Label1
         '
         resources.ApplyResources(Me.Label1, "Label1")
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(22, 50)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(41, 15)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Label1"
         '
         ' txtCommand
         '
         resources.ApplyResources(Me.txtCommand, "txtCommand")
-        Me.txtCommand.Location = New System.Drawing.Point(81, 50)
         Me.txtCommand.Name = "txtCommand"
-        Me.txtCommand.Size = New System.Drawing.Size(332, 23)
-        Me.txtCommand.TabIndex = 2
         '
         ' btnRun
         '
         resources.ApplyResources(Me.mnuMain, "btnRun")
-        Me.btnRun.Location = New System.Drawing.Point(22, 93)
         Me.btnRun.Name = "btnRun"
-        Me.btnRun.Size = New System.Drawing.Size(110, 31)
-        Me.btnRun.TabIndex = 3
-        Me.btnRun.Text = "btnRun"
         Me.btnRun.UseVisualStyleBackColor = True
         '
         ' txtOutput
         '
         resources.ApplyResources(Me.txtOutput, "txtOutput")
-        Me.txtOutput.Location = New System.Drawing.Point(16, 112)
         Me.txtOutput.Name = "txtCommand"
         Me.txtOutput.BackColor = SystemColors.Window
-        Me.txtOutput.Size = New System.Drawing.Size(416, 168)
-        Me.txtOutput.TabIndex = 4
         Me.txtOutput.ReadOnly = True
 
         '
@@ -132,15 +105,13 @@ Partial Class Form1
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(516, 349)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
         Me.Controls.Add(Me.btnRun)
         Me.Controls.Add(Me.txtCommand)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.mnuMain)
         Me.MainMenuStrip = Me.mnuMain
         Me.Name = "Form1"
-        Me.Text = "Form1"
 
         Me.mnuMain.ResumeLayout(False)
         Me.mnuMain.PerformLayout()
